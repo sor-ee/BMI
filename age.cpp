0aa2f9e9603648380c27ad3ee98eb55e99f12682
sor-ee <68268283+sor-ee@users.noreply.github.com>
2020-07-31 10:34:56 +0700

    Add files via upload

---
+++ age.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+void cal_age(string ,int); //ประกาศฟังก์ชั่น
+int main()
+{
+	int birth_year;
+	string name;
+	cout << "Enter your name : ";
+	cin >> name;
+	cout << "Enter your birth_year : ";
+	cin >> birth_year;
+	cal_age(name,birth_year); //เรียกใช้ฟังก์ชั่น
+	return 0;
+}
+void cal_age(string name,int birth_year) //สร้างฟังก์ชั่น
+{
+	cout << name <<" age = " << 2563-birth_year << " years old. " << endl;
+} 
