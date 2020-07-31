fdb7fe7b794e7125ef03b94959d974b3da78618a
sor-ee <68268283+sor-ee@users.noreply.github.com>
2020-07-31 10:34:23 +0700

    Add files via upload

---
+++ week3.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+void cal_func(int,int);//ประกาศฟังก์ชั่น
+void sub(int,int);//ประกาศฟังก์ชั่น
+void mul(int ,int);//ประกาศฟังก์ชั่น
+void dev(int ,int);//ประกาศฟังก์ชั่น
+int main()
+{
+	int x,y;
+	//int a,b;
+	cout <<"Enter number : ";
+	cin >> x;
+	cout <<"Enter number : ";
+	cin >> y;
+	cal_func(x,y);//เรียกใช้ฟังก์ชั่น
+	sub(x,y);//เรียกใช้ฟังก์ชั่น
+	mul(x,y);//เรียกใช้ฟังก์ชั่น
+	dev(x,y);//เรียกใช้ฟังก์ชั่น
+	return 0;
+}
+void cal_func(int x, int y) //การสร้างฟังก์ชั่น
+{
+	cout << x <<" + " << y << " = " << x+y << endl;
+}
+void sub(int x, int y) //การสร้างฟังก์ชั่น
+{
+	cout << x <<" - " << y << " = " << x-y << endl;
+}
+void mul(int x, int y) //การสร้างฟังก์ชั่น
+{
+	cout << x <<" * " << y << " = " << x*y << endl;
+}
+void dev(int x, int y) //การสร้างฟังก์ชั่น
+{
+	cout << x <<" / " << y << " = " << x/y << endl;
+}
